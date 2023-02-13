e3c42b601a88f295948009ba8441b2ace5708e65
o-rchid <ozzve35@gmail.com>
2023-02-13 20:02:50 +0900

    Mrp 모의전개 모달 띄움

--- src/erp/logistic/production/page/MRP/MrpRegister2.js
+++ src/erp/logistic/production/page/MRP/MrpRegister2.js
@@ -1,40 +1,24 @@
 import React, { useState, useCallback, memo, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-    Paper,
-    TextField,
-    Button,
-    Grid,
-    AppBar,
-    InputLabel,
-    Typography,
-    Toolbar,
-    MenuItem,
-    Select,
-    FormControl,
-    OutlinedInput,
-    NativeSelect
-} from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import MyGrid from 'util/LogiUtil/MyGrid';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import { searchContractDetailInMpsAvailable } from '../MPS/mpsAxios';
 import contractlistcolumn from '../MPS/contractListColumn';
-import useInput from 'util/useInput';
 import { today } from 'erp/hr/util/lib';
-import { useThemeSwitcher } from 'mui-theme-switcher';
 import MrpDialog from './MrpDialog2';
 import Swal from 'sweetalert2';
 import MainCard from '../../../../../template/ui-component/cards/MainCard';
 import MyCalendar from '../../../../../util/LogiUtil/MyCalendar';
 import { getDatePicker } from '../../../../hr/util/datePicker';
-import contractListColumn from '../MPS/contractListColumn';
 import MyDialog from '../../../../../util/LogiUtil/MyDialog';
 
 const MrpRegister = () => {
+    //그리드 값 선택
     const [checkData, setCheckData] = useState(null);
+    //MrpDialog open
     const [mrpDialog, setMrpDialog] = useState(false);
-    const [gridApi, setGridApi] = useState(null);
     const [contractGridApi, setcontractGridApi] = useState();
     //시작일 종료일
     const [calendarDate, setCalendarDate] = useState({
@@ -43,6 +27,11 @@ const MrpRegister = () => {
     });
     //그리드 데이터 - 수주 상세
     const [contractList, setContractList] = useState([]);
+    //MrpDialog로 넘겨줄 값
+    const mrpList = useSelector((state) => state.RootReducers.logistic.production.mpsReducer.MrpList);
+    const mrpsimulatorList = useSelector((state) => state.RootReducers.logistic.production.mrpsimulatorlist.MrpSimulatorList);
+    const mrpgetList = useSelector((state) => state.RootReducers.logistic.production.mrplist.MrpGetList);
+    const getherList = useSelector((state) => state.RootReducers.logistic.production.gatherlist.GatherList);
 
     //------------------이벤트----------------------
 
@@ -76,13 +65,15 @@ const MrpRegister = () => {
         [checkData]
     );
 
+    //달력 값 바꼈을 때 실행
     const onChangeDate = (e) => {
         let nextCalendarDate = { ...calendarDate };
         nextCalendarDate[e.target.id] = e.target.value;
         setCalendarDate(nextCalendarDate);
     };
 
-    const orderGirdApi = (params) => {
+    //그리드 API 가져오기
+    const gridApi = (params) => {
         setcontractGridApi(params.api);
     };
 
@@ -114,13 +105,20 @@ const MrpRegister = () => {
                     onCellClicked={undefined}
                     onRowSelected={onRowSelected}
                     rowSelection="single"
-                    api={orderGirdApi}
+                    api={gridApi}
                     components={{ datePicker: getDatePicker() }}
                 />
 
                 <MyDialog open={mrpDialog} close={mrpClose} maxWidth={'90%'}>
                     <div>
-                        <MrpDialog />
+                        <MrpDialog
+                            searchMrpList={mrpList}
+                            checkData={checkData}
+                            serCheckData={setCheckData}
+                            MrpSimulatorList={mrpsimulatorList}
+                            MrpRegisterList={mrpgetList}
+                            mrpRegisterGridApi={gridApi}
+                        />
                     </div>
                 </MyDialog>
             </MainCard>
